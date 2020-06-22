4dc09bed1680d3125ea60cd19f7e2f3d104504a2
manikandanraji <manikandan4871113@gmail.com>
2020-06-22 15:19:28 +0530

    suggestions user

--- src/components/Suggestions.js
+++ src/components/Suggestions.js
@@ -108,7 +108,7 @@ const Suggestions = () => {
 
       <div className="suggestions">
         <h3>Suggestions For You</h3>
-        {users.map((user) => (
+        {users.slice(0, 4).map((user) => (
           <div key={user.username} className="suggestions-usercard">
             <UserCard user={user} />
             <Follow nobtn isFollowing={user.isFollowing} userId={user._id} />
